3ae3fd396f76c86e6aeb7a5f220f24ef9e88b8f9
Harsh Mahajan <harshmahajan2345@gmail.com>
2024-06-23 00:28:57 +0530

    hi

--- src/components/Happy.tsx
+++ src/components/Happy.tsx
@@ -14,7 +14,6 @@ const Happy = () => {
         <div className="text-center text-neutral-700 text-lg md:text-2xl lg:text-3xl font-bold tracking-wider">Brand Design</div>
         <Image src={star} alt="star" className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
         <div className="text-center text-neutral-700 text-lg md:text-2xl lg:text-3xl font-bold tracking-wider">Graphic Design</div>
-        <Image src={star} alt="star" className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
       </div>
       <ScrollCards/>
     </div>
